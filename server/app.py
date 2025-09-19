fcc0749b374a400e9220ea623e2e2350c543aa71
Jennifer <jenninyambu@gmail.com>
2025-09-19 23:55:51 +0300

    all changes

--- server/app.py
+++ server/app.py
@@ -1,33 +1,22 @@
-#!/usr/bin/env python3
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from models import db, User, Recipe   # now safe, since db is defined in models
 
-from flask import request, session
-from flask_restful import Resource
-from sqlalchemy.exc import IntegrityError
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-from config import app, db, api
-from models import User, Recipe
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
-class Signup(Resource):
-    pass
+    @app.route("/")
+    def index():
+        return "Flask app is running!"
 
-class CheckSession(Resource):
-    pass
+    return app
 
-class Login(Resource):
-    pass
-
-class Logout(Resource):
-    pass
-
-class RecipeIndex(Resource):
-    pass
-
-api.add_resource(Signup, '/signup', endpoint='signup')
-api.add_resource(CheckSession, '/check_session', endpoint='check_session')
-api.add_resource(Login, '/login', endpoint='login')
-api.add_resource(Logout, '/logout', endpoint='logout')
-api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app = create_app()
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,15 +1,28 @@
-from sqlalchemy.orm import validates
-from sqlalchemy.ext.hybrid import hybrid_property
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
-from config import db, bcrypt
+db = SQLAlchemy()
 
 class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+
+    # relationships
+    recipes = db.relationship("Recipe", back_populates="user")
+
 
 class Recipe(db.Model, SerializerMixin):
-    __tablename__ = 'recipes'
-    
-    pass
+    __tablename__ = "recipes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    # foreign key
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    # relationships
+    user = db.relationship("User", back_populates="recipes")
